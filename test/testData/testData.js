c3f204953030ace28fae90b177c8cdbc89abf795
Muhtar194 <68969060+Muhtar194@users.noreply.github.com>
2024-11-04 10:15:58 -0600

    Add files via upload

---
+++ test/testData/testData.js
@@ -0,0 +1,41 @@
+// testdata.js
+// testdata.js
+
+module.exports = {
+  
+    requiredFields: {
+      currentAge: '40',
+      retirementAge: '68',
+      currentIncome: '100000',
+      spouseIncome: '75000',
+      totalSavings: '75000',
+      annualSavings: '40',
+      savingsIncreaseRate: '2',
+    },
+  
+   
+    socialSecurity: {
+      overrideAmount: '4000',
+    },
+  
+    
+    defaultValues: {
+      additionalIncome: '13000',
+      retirementDuration: '10',
+      expectedInflationRate: '2',
+      retirementAnnualIncome: '50',
+      preRetirementROI: '8',
+      postRetirementROI: '7',
+    },
+  
+    
+    toggleOptions: {
+      socialBenefitsYes: true,
+    },
+  
+   
+    urls: {
+      retirementCalculatorPage: 'retirement-calculator.html',
+    },
+  };
+  
